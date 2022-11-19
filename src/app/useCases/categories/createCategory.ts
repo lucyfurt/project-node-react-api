02349a0595e222517708be206f66dcdc5c921bfa
lucyfurt <lucianafurtado10@gmail.com>
2022-11-19 17:44:49 -0300

    Try/catch no Create Category

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -4,9 +4,14 @@ import { Category } from '../../models/Category';
 
 
 export async function createCategory(req: Request, res: Response){
-    const {icon, name } = req.body;
+    try{
+        const {icon, name } = req.body;
 
-    const category =  await Category.create({icon, name});
-    res.json(category);
+        const category =  await Category.create({icon, name});
+        res.status(201).json(category);
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
