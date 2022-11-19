cb0bcb19773b87e9acbed29739881d48dd2099cc
lucyfurt <lucianafurtado10@gmail.com>
2022-11-19 17:36:00 -0300

    Casos de uso do model Category

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
-
-
+import {router} from './router';
 
 mongoose.connect('mongodb+srv://lucyfurt:lulu1010@cluster0.fmmd1rt.mongodb.net/test')
     .then(()=>{
         const app = express();
         const port = '3001';
+        app.use(express.json());
+        app.use(router);
+
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
