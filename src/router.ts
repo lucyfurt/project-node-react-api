a7c5dffaa4b1347e6b3d5106b82e5ca117a147d8
lucyfurt <lucianafurtado10@gmail.com>
2022-11-20 11:03:07 -0300

    Change Status e Delete Status

--- src/router.ts
+++ src/router.ts
@@ -9,6 +9,8 @@ import { listProducts } from './app/useCases/products/listProducts';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createOrder } from './app/useCases/orders/createOrder';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { deleteOrder } from './app/useCases/orders/deleteOrder';
 export const router = Router();
 
 const upload = multer({
@@ -51,10 +53,7 @@ router.get('/orders', listOrders);
 router.post('/orders', createOrder);
 
 //change order status
-router.patch('/orders/:orderId', (req, res)=>{
-    res.send('OK');
-});
+router.patch('/orders/:orderId', changeOrderStatus);
+
 //delete/cancel order
-router.delete('/orders/:orderId', (req, res)=>{
-    res.send('OK');
-});
+router.delete('/orders/:orderId', deleteOrder);
