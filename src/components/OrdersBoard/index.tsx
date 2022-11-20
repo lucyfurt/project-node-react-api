3cc9eef2c3670e0bb22eaa1f5090906211a6c1c5
lucyfurt <lucianafurtado10@gmail.com>
2022-11-20 16:28:27 -0300

    Create OrderModal

--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Order } from '../../types/Order';
+import { OrderModal } from '../OrderModal';
 import { Board, OrdersContainer } from './styles';
 
 interface OrdersBoardProps {
@@ -9,9 +11,16 @@ orders: Order[];
 
 
 export function OrdersBoard({icon, title, orders}: OrdersBoardProps){
+  const[isModalVisible, setIsModalVisible] = useState(false);
+  function handleOpenModal(){
+    setIsModalVisible(true);
+  }
 
   return (
     <Board>
+      <OrderModal visible={isModalVisible}/>
+
+
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
@@ -21,13 +30,11 @@ export function OrdersBoard({icon, title, orders}: OrdersBoardProps){
       {orders.length>0 &&(
         <OrdersContainer>
           {orders.map ((order)=>(
-            <button type='button' key={order._id}>
+            <button type='button' key={order._id} onClick={handleOpenModal}>
               <strong>Mesa {order.table}</strong>
               <span>{order.products.length}</span>
             </button>
           ))}
-
-
         </OrdersContainer>
       )}
     </Board>
