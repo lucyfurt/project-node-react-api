79fd86dd936d14e96fac6ff8777e13a51dff0027
lucyfurt <lucianafurtado10@gmail.com>
2022-11-20 13:36:02 -0300

    Project Front-end- React

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
+import {Header} from './components/Header';
+
+export function App(){
+
+  return (
+    <React.Fragment>
+
+      <GlobalStyles/>
+      <Header/>
+
+
+    </React.Fragment>
+
+  );
+}
+
